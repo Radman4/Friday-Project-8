922df2be7e8620c10bef2aba84f88c3b7e8a9c34
Radman4 <nootnoot432@gmail.com>
2025-04-03 19:35:16 -0500

    Added much needed labels pertianing to what each entry and combobox is. should explains what each entry is for

--- FP8.py
+++ FP8.py
@@ -8,21 +8,33 @@ class GreetingApp:
         frame1 = ttk.Frame(master, height = 200, width=300)
         frame1.pack()
         #Entry for the name
+        self.label1 = ttk.Label(frame1, text="Name:")
+        self.label1.grid()
         self.name = ttk.Entry(frame1)
         self.name.grid()
         #birthday Entry
+        self.label2 = ttk.Label(frame1, text="Birthday:")
+        self.label2.grid()
         self.bday = ttk.Entry(frame1)
         self.bday.grid()
         #email Entry
+        self.label3 = ttk.Label(frame1, text="Email:")
+        self.label3.grid()
         self.email = ttk.Entry(frame1)
         self.email.grid()
         #phone number Entry
+        self.label4 = ttk.Label(frame1, text="Phone number:")
+        self.label4.grid()
         self.phone = ttk.Entry(frame1)
         self.phone.grid()
         #address Entry
+        self.label5 = ttk.Label(frame1, text="Address:")
+        self.label5.grid()
         self.address = ttk.Entry(frame1)
         self.address.grid()
         #dropdown for contact method
+        self.label6 = ttk.Label(frame1, text="Contact Method:")
+        self.label6.grid()
         self.contact = ttk.Combobox(frame1, state= "readonly",values = ["Email", "Phone", "Text"])
         self.contact.grid()
         #button to submit the information
