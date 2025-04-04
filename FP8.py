03b99407d90022d36a5de99d79a27f02a4bee9cf
Radman4 <nootnoot432@gmail.com>
2025-04-04 09:37:34 -0500

    reformatted FP8.py to have text and entries aligned in the same columns. also changed all database functions to be the example covered on april 2.

--- FP8.py
+++ FP8.py
@@ -3,8 +3,7 @@
 from tkinter import *
 from tkinter import ttk
 #allows connection to the database
-import sqlite3
-conn = sqlite3.connect('sampleDB.db')
+
 root = Tk()
 class GreetingApp:
     def __init__ (self, master):
@@ -12,40 +11,40 @@ class GreetingApp:
         frame1.pack()
         #Entry for the name
         self.label1 = ttk.Label(frame1, text="Name:")
-        self.label1.grid()
+        self.label1.grid(column= 0, row= 0)
         self.name = ttk.Entry(frame1)
-        self.name.grid()
+        self.name.grid(column= 1, row= 0)
         #birthday Entry
-        self.label2 = ttk.Label(frame1, text="Birthday:")
-        self.label2.grid()
+        self.label2 = ttk.Label(frame1, text="Birthday (MM/DD/YYYY):")
+        self.label2.grid(column= 0, row = 1)
         self.bday = ttk.Entry(frame1)
-        self.bday.grid()
+        self.bday.grid(column= 1, row = 1)
         #email Entry
         self.label3 = ttk.Label(frame1, text="Email:")
-        self.label3.grid()
+        self.label3.grid(column= 0, row = 2)
         self.email = ttk.Entry(frame1)
-        self.email.grid()
+        self.email.grid(column= 1, row = 2)
         #phone number Entry
         self.label4 = ttk.Label(frame1, text="Phone number:")
-        self.label4.grid()
+        self.label4.grid(column= 0, row = 3)
         self.phone = ttk.Entry(frame1)
-        self.phone.grid()
+        self.phone.grid(column= 1, row = 3)
         #address Entry
         self.label5 = ttk.Label(frame1, text="Address:")
-        self.label5.grid()
+        self.label5.grid(column= 0, row = 4)
         self.address = ttk.Entry(frame1)
-        self.address.grid()
+        self.address.grid(column= 1, row = 4)
         #dropdown for contact method
         self.label6 = ttk.Label(frame1, text="Contact Method:")
-        self.label6.grid()
+        self.label6.grid(column= 0, row = 5)
         self.contact = ttk.Combobox(frame1, state= "readonly",values = ["Email", "Phone", "Text"])
-        self.contact.grid()
+        self.contact.grid(column= 1, row = 5)
         #button to submit the information
         self.btn1 = ttk.Button(frame1, text="Submit")
         self.btn1.config(command=self.method1)
         self.btn1.grid()
     def method1(self):
         #should be that the sampleDB is given a new set of values
-        print("Hello WORLD")
+        print("test")
 app = GreetingApp(root)
 root.mainloop()
